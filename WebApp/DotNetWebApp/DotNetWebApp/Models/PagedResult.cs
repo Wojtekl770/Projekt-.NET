7721688aabf27548ac23a2d0c0ee081b1a60991e
Agata Steciuk <01180836@pw.edu.pl>
2025-01-16 00:39:32 +0100

    FrontEnd - walka nocą

--- WebApp/DotNetWebApp/DotNetWebApp/Models/PagedResult.cs
+++ WebApp/DotNetWebApp/DotNetWebApp/Models/PagedResult.cs
@@ -7,6 +7,5 @@
         public int PageSize { get; set; }
         public int CurrentPage { get; set; }
         public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
-
     }
 }
